e25a15e025bebf6f3f2d9e3307024758a13d6e2a
nostojic <nikolaostoji@gmail.com>
2018-04-08 18:00:31 -0400

    led refactor

--- SiemensTestProgram/DeviceManager/ComCommands.cs
+++ SiemensTestProgram/DeviceManager/ComCommands.cs
@@ -2,42 +2,13 @@
 
 namespace DeviceManager
 {
-    using Common;
-
     public static class ComCommands
     {
-        /// <summary>
-        /// Types of commands.
-        /// </summary>
-        public enum CommandType
-        {
-            LED,
-            DAC,
-            TEC,
-            Unknown
-        }
-
         // Register read/write
         public const byte REGISTER_READ = 0x01;
         public const byte REGISTER_WRITE = 0x02;
 
-        // Gets the array for LED command        
-        public static byte[] GetLedCommand(byte rw, byte led)
-        {
-            return new byte[] 
-            {
-                rw,
-                LED_ADDRESS[0],
-                LED_ADDRESS[1],
-                LED_ADDRESS[2],
-                LED_ADDRESS[3],
-                0x00,
-                0x00,
-                0x00,
-                led
-            };
-        }
-
+        
         // Gets the array for DAC command   
         public static byte[] GetDacCommand(byte rw, byte[] value)
         {
@@ -59,79 +30,5 @@ namespace DeviceManager
                 value[3]
             };
         }
-
-        // Gets the array for ADC control command
-        public static byte[] GetAdcControlCommand(byte rw, int channel, int startAdc)
-        {
-            byte adcChannel = Helper.IntegerToByte(channel);
-            byte startCommunication = Helper.IntegerToByte(startAdc);
-
-            var adcCommand = new byte[9]
-            {
-                rw,
-                0x00,
-                0x00,
-                0x01,
-                0x00,
-                0x00,
-                0x00,
-                startCommunication,
-                adcChannel
-            };
-
-            return adcCommand;
-        }
-
-        // Gets the array for ADC status command
-        public static byte[] GetAdcStatusCommand()
-        {
-            return new byte[]
-            {
-                REGISTER_READ,
-                0x00,
-                0x00,
-                0x01,
-                0x01,
-                0x00,
-                0x00,
-                0x00,
-                0x02
-            };
-        }
-
-        // Gets the array for ADC result command
-        public static byte[] GetAdcResultCommand()
-        {
-            return new byte[]
-            {
-                REGISTER_READ,
-                0x00,
-                0x00,
-                0x01,
-                0x02,
-                0x00,
-                0x00,
-                0x00,
-                0x02
-            };
-        }
-
-        // LED ADDRESS
-        public static byte[] LED_ADDRESS = new byte[4]
-        {
-            0x00,
-            0x00,
-            0x09,
-            0x00
-        };
-
-        // SWITCH ADDRESS
-        public static byte[] SWITCH_ADDRESS = new byte[4]
-        {
-            0x00,
-            0x00,
-            0x00,
-            0x01
-        };
     }
 }
--- SiemensTestProgram/DeviceManager/Model/ILedModel.cs
+++ SiemensTestProgram/DeviceManager/Model/ILedModel.cs
@@ -2,7 +2,6 @@
 
 namespace DeviceManager.Model
 {
-    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     /// <summary>
@@ -11,36 +10,10 @@ namespace DeviceManager.Model
     public interface ILedModel
     {
         /// <summary>
-        /// Writes data through the comp port.
+        /// Writes to LED request.
         /// </summary>
-        /// <param name="request"> Sends a request. </param>
-        /// <returns> Task which returns byte array of response when completed. </returns>
-        Task<byte[]> WriteData(byte[] request);
-
-        ///// <summary>
-        ///// Reconfigures the com port settings in the communication class.
-        ///// </summary>
-        ///// <param name="comPort"> Com port </param>
-        ///// <param name="baudRate"> Baud rate </param>
-        ///// <param name="dataBits"> Data bits </param>
-        ///// <param name="parity"> Parity </param>
-        ///// <param name="stopBits"> Stop bits </param>
-        //void ReconfigureComCommunication(string comPort, int baudRate, int dataBits, System.IO.Ports.Parity parity, System.IO.Ports.StopBits stopBits);
-
-        ///// <summary>
-        ///// Gets the comp ports available.
-        ///// </summary>
-        ///// <returns> Available com ports. </returns>
-        //List<string> GetPortSettings();
-
-        ///// <summary>
-        ///// Gets the default selected settings for serial communication.
-        ///// </summary>
-        ///// <param name="selectedComPort"> Comp port </param>
-        ///// <param name="selectedBaudRate"> Baud rate </param>
-        ///// <param name="dataBits"> Data bits </param>
-        ///// <param name="selectedParity"> Selected parity </param>
-        ///// <param name="selectedStopBits"> Stop bits </param>
-        //void GetDefaultSettings(ref string selectedComPort, ref int selectedBaudRate, ref int dataBits, ref System.IO.Ports.Parity selectedParity, ref System.IO.Ports.StopBits selectedStopBits);
+        /// <param name="request"> LED toggle status. </param>
+        /// <returns> Task which will return LED register response. </returns>
+        Task<byte[]> SetLedCommand(string ledRequest);
     }
 }
