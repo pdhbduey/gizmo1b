c7f0f431911ead9473430544e003734f0e5796d1
nostojic <nikolaostoji@gmail.com>
2018-04-14 21:31:03 -0400

    save

--- SiemensTestProgram/DeviceManager/MotorDefaults.cs
+++ SiemensTestProgram/DeviceManager/MotorDefaults.cs
@@ -178,6 +178,35 @@ namespace DeviceManager
             0x00
         };
 
+        public static List<string> RegisterAddresses = new List<string>
+        {
+            "ABS_POS",
+            "EL_POS",
+            "MARK",
+            "SPEED",
+            "ACC",
+            "DEC",
+            "MAX_SPEED",
+            "MIN_SPEED",
+            "FS_SPD",
+            "KVAL_HOLD",
+            "KVAL_RUN",
+            "KVAL_ACC",
+            "KVAL_DEC",
+            "INT_SPEED",
+            "ST_SLP",
+            "FN_SLP_ACC",
+            "FN_SLP_DEC",
+            "K_THERM",
+            "ADC_OUT",
+            "OCD_TH",
+            "STALL_TH",
+            "STEP_MODE",
+            "ALARM_EN",
+            "CONFIG",
+            "STATUS"
+        };
+
         public static Dictionary<string, byte> DirectionValues = new Dictionary<string, byte>()
         {
             { Directions[0], 0x08},
@@ -196,6 +225,41 @@ namespace DeviceManager
             { StepSizes[7], GetStepModeValue(8)}
         };
 
+        public static Dictionary<string, byte> RegisterAddressValues = new Dictionary<string, byte>()
+        {
+            {RegisterAddresses[0], 0x02 },
+            {RegisterAddresses[0], 0x03 },
+            {RegisterAddresses[0], 0x04 },
+            {RegisterAddresses[0], 0x05 },
+            {RegisterAddresses[0], 0x06 },
+            {RegisterAddresses[0], 0x07 },
+            {RegisterAddresses[0], 0x08 },
+            {RegisterAddresses[0], 0x15 },
+            {RegisterAddresses[0], 0x09 },
+            {RegisterAddresses[0], 0x0a },
+            {RegisterAddresses[0], 0x0b },
+            {RegisterAddresses[0], 0x0c },
+            {RegisterAddresses[0], 0x0d },
+            {RegisterAddresses[0], 0x0e },
+            {RegisterAddresses[0], 0x0f },
+            {RegisterAddresses[0], 0x10 },
+            {RegisterAddresses[0], 0x11 },
+            {RegisterAddresses[0], 0x12 },
+            {RegisterAddresses[0], 0x13 },
+            {RegisterAddresses[0], 0x14 },
+            {RegisterAddresses[0], 0x16 },
+            {RegisterAddresses[0], 0x17 },
+            {RegisterAddresses[0], 0x18 },
+            {RegisterAddresses[0], 0x19 },
+            {RegisterAddresses[0], 0x01 },
+            {RegisterAddresses[0], 0x01 },
+            {RegisterAddresses[0], 0x01 },
+            {RegisterAddresses[0], 0x01 },
+            {RegisterAddresses[0], 0x01 },
+            {RegisterAddresses[0], 0x01 },
+        };
+
+             
         public static byte[] GetStepModeValue(int mode)
         {
             var value = new byte[2];
