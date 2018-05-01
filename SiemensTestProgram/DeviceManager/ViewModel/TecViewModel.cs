2a733ffbdb49937d06279cf5f33b36fd886edaee
nostojic <nikolaostoji@gmail.com>
2018-05-01 16:05:11 -0400

    warning fix

--- SiemensTestProgram/DeviceManager/ViewModel/TecViewModel.cs
+++ SiemensTestProgram/DeviceManager/ViewModel/TecViewModel.cs
@@ -25,7 +25,7 @@ namespace DeviceManager.ViewModel
         private int numberOfSamples;
         private float irefCurrentValue;
         private int sliderIrefValue;
-        private int irefGain;
+        //private int irefGain;
         private int tecPeriod;
         private int counter;
         private int waveformCycles;
@@ -51,6 +51,8 @@ namespace DeviceManager.ViewModel
             this.tecModel = tecModel;
 
             // Initial values
+            saving = false;
+            updating = false;
             counter = 0;
             progressMaximum = 100;
             tecPeriod = TecDefaults.PeriodMinimum;
